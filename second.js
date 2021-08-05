7b9d8cf2ff59a7c2df61c555ec00386f1b1fb075
Vanya01 <good.boy.2k19@gmail.com>
2021-08-05 17:48:07 +0300

    day 4

--- second.js
+++ second.js
@@ -1,6 +1,7 @@
 // 5 Добавить кнопку "post of current user", при клике на которую, появляются title всех постов текущего юзера
 // На странице user-details.html:
 // (для получения постов используйте эндпоинт https://jsonplaceholder.typicode.com/users/USER_ID/posts)
+// 6 Каждому посту добавить кнопку/ссылку, при клике на которую происходит переход на страницу post-details.html, которая имеет детальную информацию про текущий пост.
 
 const mainBlock = document.getElementById('second_page')
 const url = new URL(location)
@@ -8,46 +9,57 @@ const join = url.searchParams.get('user')
 const user = JSON.parse(join)
 
 let Div = document.createElement('div')
-Div.innerText = ` Id - ${user.id}
-Name - ${user.name}
-Username - ${user.username}
-Email - ${user.email}
-Street - ${user.address.street}
-Zipcode - ${user.address.zipcode}
-Lat -  ${user.address.geo.lat}
-Lng  - ${user.address.geo.lng}
-Phone  - ${user.phone}
-Website -  ${user.website}
-Company name- ${user.company.name}
-Company cash - ${user.company.catchPhrase}
-Company BS - ${user.company.bs}
+Div.classList.add('secondDiv')
+Div.innerHTML = `<div id="wrap_info">
+
+<div class="wrapper">
+<div class="wrapper_in"><h3>ID:</h3><p>${user.id}</p></div>
+<div class="wrapper_in"><h3> Name:</h3><p>${user.name}</p></div>
+<div class="wrapper_in"><h3>Username:</h3><p>${user.username}</p></div>
+<div class="wrapper_in"><h3>Email:</h3><p>${user.email}</p></div>
+<div class="wrapper_in"><h3>Street:</h3><p>${user.address.street}</p></div>
+<div class="wrapper_in"><h3>Zipcode:</h3><p>${user.address.zipcode}</p></div>
+<div class="wrapper_in"><h3>Lat:</h3><p>${user.address.geo.lat}</p></div>
+<div class="wrapper_in"><h3>Lng:</h3><p>${user.address.geo.lng}</p></div>
+<div class="wrapper_in"><h3>Phone:</h3><p>${user.phone}</p></div>
+<div class="wrapper_in"><h3>Website:</h3><p>${user.website}</p></div>
+<div class="wrapper_in"><h3>Company name:</h3><p>${user.company.name}</p></div>
+<div class="wrapper_in"><h3>Company cash:</h3><p>${user.company.catchPhrase}</p></div>
+<div class="wrapper_in"><h3>Company BS:</h3><p>${user.company.bs}</p></div>
+
+
+</div>
+</div>
 `
 
 let btn_two = document.createElement('button')
+btn_two.classList.add('second_button')
 btn_two.innerText = 'Posts of current user'
 
 btn_two.onclick = function () {
-
-    const empty = document.createElement('p')
 
       fetch(`https://jsonplaceholder.typicode.com/users/${user.id}/posts`)
      .then(value => value.json())
      .then(posts => {
          for (const post of posts){
-            const block = document.getElementById('two')
-             block.innerText = ''
-               const empty_p = document.createElement('p')
-               const empty_title = document.createElement('h4')
-             empty_title.innerText = `${post.id} : ${post.title}`
+             const block = document.getElementById('two')
+             const empty_div_two = document.createElement('div')
+              empty_div_two.classList.add('post_wrap')
+              const empty_p = document.createElement('p')
+               empty_p.classList.add('posts_p')
+              const empty_title = document.createElement('h4')
+               empty_title.classList.add('posts_block')
+              empty_title.innerText = `${post.id} : ${post.title}`
               empty_p.innerText = `${post.body}`
-             let btn_second = document.createElement('button')
-             btn_second.innerText = 'Post details'
-
-             btn_second.href = `Post-details?post=${JSON.stringify(post)}`
-
-             block.append(empty_title,empty_p,btn_second)
+              let btn_second = document.createElement('button')
+              btn_second.classList.add('posts_btn')
+              btn_second.innerText = 'Post details'
+             btn_second.onclick = () => {
+                location.href = `Post-details.html?post=${JSON.stringify(post)}`
+             }
+              block.append(empty_div_two)
+             empty_div_two.append(empty_title,empty_p,btn_second)
              mainBlock.appendChild(block)
-
          }
          })
 }
@@ -55,5 +67,4 @@ mainBlock.append(Div)
 mainBlock.appendChild(btn_two)
 
 
-// 6 Каждому посту добавить кнопку/ссылку, при клике на которую происходит переход на страницу post-details.html, которая имеет детальную информацию про текущий пост.
 
---
+++ third.js
@@ -0,0 +1,26 @@
+// На странице post-details.html:
+// 7 Вывести всю, без исключения, информацию про объект post на кнопку/ссылку которого был совершен клик ранее.
+// 8 Ниже информации про пост, вывести все комментарии текущего поста (эндпоинт для получения информации - https://jsonplaceholder.typicode.com/posts/POST_ID/comments)
+
+const  mainDiv = document.getElementById('block_three')
+const get = document.getElementById('last_wrap')
+const url = new URL(location)
+const join = url.searchParams.get('post')
+const post = JSON.parse(join)
+
+let postId =  document.getElementsByClassName('pg').innerText = `${post.id}`
+let postBody =  document.getElementsByClassName('com_wrap').innerText = `${post.body}`
+
+
+fetch(`https://jsonplaceholder.typicode.com/posts/${post.id}/comments`)
+    .then(value => value.json())
+    .then(comments => {
+        for (const comment of comments) {
+            let threeDiv = document.createElement('div')
+            threeDiv.classList.add('com_div')
+            threeDiv.innerText = 'Comment:' + ' ' + comment.body
+            mainDiv.appendChild(threeDiv)
+        }
+        mainDiv.append(postId)
+        get.append(mainDiv)
+    })
